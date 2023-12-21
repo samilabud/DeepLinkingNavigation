e4e198043c4d058a6aa6c6cf05916357fbe1e39c
Samil Abud <samilabud@gmail.com>
2023-12-20 21:46:21 -0400

    First commit, added react native app

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import {Text} from "react-native";
+import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
+import * as Linking from "expo-linking";
 
+const prefix = Linking.createURL("/");
+
+const linking = {
+  prefixes: [prefix],
+  config: {
+    screens: {
+      Home: "home",
+      Settings: "settings",
+    },
+  },
+};
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer fallback={<Text>Loading...</Text>}>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
