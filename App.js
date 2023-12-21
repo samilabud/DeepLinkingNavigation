5eeff157b04d6f70ea34516f29a48e714432a934
Samil Abud <samilabud@gmail.com>
2023-12-20 21:55:44 -0400

    Added screens and not found functionality

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
+import NotFoundScreen from "./screens/NotFound";
 import * as Linking from "expo-linking";
 
 const prefix = Linking.createURL("/");
@@ -14,6 +15,7 @@ const linking = {
     screens: {
       Home: "home",
       Settings: "settings",
+      NotFound: "*",
     },
   },
 };
@@ -25,7 +27,12 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="NotFound" component={NotFoundScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+//For testing deep links
+// npx uri-scheme open exp://10.0.0.27:19000/--/home --android
+// npx uri-scheme open exp://10.0.0.27:19000/--/whatever --android
+// npx uri-scheme open exp://10.0.0.27:19000/--/settings --android
--- screens/NotFound.js
+++ screens/NotFound.js
@@ -2,16 +2,16 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
-const SettingsScreen = () => {
+const NotFoundScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Settings-Screen</Text>
+      <Text>Not Found</Text>
       <StatusBar style="auto" />
     </View>
   );
 };
 
-export default SettingsScreen;
+export default NotFoundScreen;
 
 const styles = StyleSheet.create({
   container: {
